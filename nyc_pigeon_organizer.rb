b90ef4fa7ab51c206e3d523dabd4dec159959970
Matine temory <temorymatine@gmail.com>
2020-05-27 23:51:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+data.each_with_object({}) do | (key, value),  final_array |
+  value.each do | innerkey, names |
+    names.each do |name|
+      if !final_array[name]
+        final_array[name]= {}
+        
+      end
+      
+      if !final_array[name][key]
+          final_array[name][key] = []
+        end
+        if
+  
 end
