961c676019d3bc640dfdf904c30563d3c4de3b49
Matine temory <temorymatine@gmail.com>
2020-05-27 23:52:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,8 +9,11 @@ data.each_with_object({}) do | (key, value),  final_array |
       end
       
       if !final_array[name][key]
-          final_array[name][key] = []
+          !final_array[name][key] = []
         end
         final_array[name][key].push[inner_key.to_s]
+      end
+    end
+  end
   
 end
